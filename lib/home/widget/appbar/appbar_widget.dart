144652130d1d8e9d2995010d0eb623e2905f9636
Marcos Koch Salvador <marcosks0@gmail.com>
2021-04-20 00:51:28 -0300

    Primeira aula

---
+++ lib/home/widget/appbar/appbar_widget.dart
@@ -0,0 +1,49 @@
+import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/home/widget/score_card/score_card_widget.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppBarWidget extends PreferredSize {
+  
+  AppBarWidget() : super(
+    preferredSize: Size.fromHeight(250),
+    child: Container(
+      height: 250,
+      child: Stack(
+        children: [
+          Container(
+            height: 161,
+            width: double.maxFinite,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(gradient: AppGradients.linear),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(TextSpan(
+                  text: 'Olá, ',
+                  style: AppTextStyles.title,
+                  children: [
+                    TextSpan(text: 'Marcos', style: AppTextStyles.titleBold)
+                  ]
+                )),
+                Container(
+                  width: 58,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(image: NetworkImage('https://avatars.githubusercontent.com/u/1117948?v=4'))
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ScoreCardWidget()
+          )
+        ],
+      ),
+    )
+  );
+
+}
